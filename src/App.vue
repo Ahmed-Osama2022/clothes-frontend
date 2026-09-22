8560c05f842761291461fed6a5ad95c4d251dedb
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2026-09-22 22:29:46 +0300

    feat: implement routing and add initial views for Home and About

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,3 @@
 <template>
-  <main class="flex min-h-screen items-center justify-center bg-neutral-100">
-    <h1 class="text-3xl font-bold text-neutral-800">Clothes Frontend</h1>
-  </main>
+  <router-view />
 </template>
